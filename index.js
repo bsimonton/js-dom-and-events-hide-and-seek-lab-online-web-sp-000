8b3e2063471af8b26c993205a6aff5f6f7861679
Brian Simonton <bsimonton78@gmail.com>
2019-07-28 13:26:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,12 @@ function deepestChild(){
 function increaseRankBy(n){
   
   const lis = document
-  .querySelectorAll('.ranked-list li')
+  .querySelectorAll('.ranked-list li');
+  
+  for (let i = 0; i < lis.length; i++){
+    
+    
+  }
   
 }
 
